c859000969c7ada70d74aad675450a10c90590bf
SohailaMohammd <160627240+SohailaMohammd@users.noreply.github.com>
2024-12-25 14:21:02 +0200

    newUpdate

--- src/components/model/selectImage/SelectImage.jsx
+++ src/components/model/selectImage/SelectImage.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useImageFile } from "../../../context/ImageFileProvider";
 import { MdCloudUpload } from "react-icons/md";
+
 import "./selectImage.css";
 
 const SelectImage = ({ imageType }) => {
@@ -21,13 +22,26 @@ const SelectImage = ({ imageType }) => {
   };
 
   return (
-    <div className="upload">
-      <input type="file" onChange={handleImageChange} id="image-file" />
-      <div className="upload-text">
-        <MdCloudUpload size={50} color="#777" />
-        <p>Click to select or drag and drop image</p>
+    <>
+      <div className="upload">
+        <input type="file" onChange={handleImageChange} id="image-file" />
+        <div className="upload-text">
+          {/* <MdCloudUpload size={50} color="#777" />
+        <p>Click to select or drag and drop image</p> */}
+          <div className="icon-container">
+            <i className="fa-regular fa-image"></i>
+            <i className="fa-solid fa-plus"></i>
+          </div>
+          <p>Tap to drag your Design</p>
+        </div>
+
       </div>
-    </div>
+      <div className="innovation">
+        <i className="fa-solid fa-lightbulb"></i>
+        <p className="idea">Good designs bring life. Make your design speak for you, and let our smart recommendations guide your creativity.</p>
+
+      </div>
+    </>
   );
 };
 
